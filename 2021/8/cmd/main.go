27756facd12001c37d4771f0233a5e5296b20552
Emanuele Menon <emnlmn@gmail.com>
2021-12-08 12:35:35 +0100

    solve day8 part 2
    Signed-off-by: Emanuele Menon <emnlmn@gmail.com>

--- 2021/8/cmd/main.go
+++ 2021/8/cmd/main.go
@@ -7,41 +7,25 @@ import (
 	"os"
 	"path"
 	"runtime"
+	"sort"
 	"strings"
 )
 
 func main() {
-	_, digits, err := readInput()
-
-	digitLengthMap := map[int]int {
-		2: 1,
-		4: 4,
-		3: 7,
-		7: 8,
-	}
+	total, err := parseInput()
 
 	if err != nil {
 		panic(err)
 	}
 
-
-
-	digitCount := 0
-
-	for _, digitSegment := range digits {
-		if _, ok := digitLengthMap[len(digitSegment)]; ok {
-			digitCount += 1
-		}
-	}
-
-	fmt.Println(digitCount)
+	fmt.Println(total)
 }
 
-func readInput() (segments []string, digits []string, err error) {
+func parseInput() (total int, err error) {
 	_, filename, _, ok := runtime.Caller(1)
 
 	if !ok {
-		return segments, digits, errors.New("could not get runtime path")
+		return total, errors.New("could not get runtime path")
 	}
 
 	filepath := path.Join(path.Dir(filename), "../input.txt")
@@ -49,7 +33,7 @@ func readInput() (segments []string, digits []string, err error) {
 	file, err := os.Open(filepath)
 
 	if err != nil {
-		return segments, digits, err
+		return total, err
 	}
 
 	defer file.Close()
@@ -60,14 +44,113 @@ func readInput() (segments []string, digits []string, err error) {
 		line = strings.Trim(line, " ")
 		parts := strings.Split(line, " | ")
 
-		for _, seg := range strings.Split(parts[0], " ") {
-			segments = append(segments, seg)
-		}
+		inputs := sortS(strings.Split(parts[0], " "))
+		outputs := sortS(strings.Split(parts[1], " "))
 
-		for _, seg := range strings.Split(parts[1], " ") {
-			digits = append(digits, seg)
+		total += decode(inputs, outputs)
+	}
+
+	return total, err
+}
+
+func decode(input []string, output []string) (total int) {
+	digitLengthMap := map[int]int {
+		2: 1,
+		4: 4,
+		3: 7,
+		7: 8,
+	}
+	encoding := map[int]string{}
+
+	//      aaaa
+	//     b    c
+	//     b    c
+	//      dddd
+	//     e    f
+	//     e    f
+	//      gggg
+
+	for _, digitSegment := range input {
+		if digit, ok := digitLengthMap[len(digitSegment)]; ok {
+			encoding[digit] = digitSegment
 		}
 	}
 
-	return segments, digits, err
+	for _, digitSegment := range input {
+		if len(digitSegment) == 5 && len(leftBisect(encoding[1], digitSegment)) == 0 {
+			encoding[3] = digitSegment
+			continue
+		}
+
+		if len(digitSegment) == 6 {
+			switch {
+			// Solve for 0, 6, and 9
+			case len(leftBisect(encoding[1], digitSegment)) == 1:
+				encoding[6] = digitSegment
+			case len(leftBisect(encoding[4], digitSegment)) == 0:
+				encoding[9] = digitSegment
+			case len(leftBisect(encoding[4], digitSegment)) == 1:
+				encoding[0] = digitSegment
+			}
+		}
+	}
+
+
+	rightTop := leftBisect(encoding[1], encoding[6])[0]
+
+	for _, digitSegment := range input {
+		if len(digitSegment) != 5 || digitSegment == encoding[3] {
+			continue
+		}
+
+		if strings.Contains(digitSegment, string(rightTop)) {
+			encoding[2] = digitSegment
+		} else {
+			encoding[5] = digitSegment
+		}
+	}
+
+	for _, outputSegment := range output {
+		total *= 10
+		for k, v := range encoding {
+			if v == outputSegment {
+				total += k
+			}
+		}
+	}
+
+	return total
+}
+
+// returns chars from a not present in b
+func leftBisect(a, b string) string {
+	result := strings.Builder{}
+
+search:
+	for _, aa := range a {
+		for _, bb := range b {
+			if aa == bb {
+				continue search
+			}
+		}
+
+		result.WriteRune(aa)
+	}
+
+	return result.String()
+}
+
+func sortS(s []string) []string {
+	result := make([]string, len(s))
+
+	for i, str := range s {
+		v := []rune(str)
+		sort.Slice(v, func(i, j int) bool {
+			return v[i] < v[j]
+		})
+
+		result[i] = string(v)
+	}
+
+	return result
 }
